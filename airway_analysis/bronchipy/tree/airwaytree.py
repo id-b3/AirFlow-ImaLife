2b7d0ecce0e824c87cd78d356102bbd37dac2c65
id-b3 <58172188+id-b3@users.noreply.github.com>
2021-06-09 18:14:27 +0200

    Added value describing number of points to the dataframe.

--- airway_analysis/bronchipy/tree/airwaytree.py
+++ airway_analysis/bronchipy/tree/airwaytree.py
@@ -21,6 +21,8 @@ class AirwayTree:
         branch_df = brio.load_branch_csv(self.files['branch'])
         # Apply the voxel dimensions to the points and create a data entry containing the centreline points in mm.
         branch_df['centreline'] = branch_df.apply(lambda row: [self.vox_to_mm(points) for points in row.points], axis=1)
+        # Add entry describing the number of points in the airway measurement.
+        branch_df['num_points'] = branch_df.apply(lambda row: len(row.points), axis=1)
 
         # Load inner measurements csvs into dataframes
         inner_df = brio.load_csv(self.files['inner'], True)
@@ -59,6 +61,7 @@ class AirwayTree:
         Returns
         ----------
         AirwayTree Object containing volume information and the airway tree data.
+
         """
         self.files = {'branch': branch_file, 'inner': inner_file, 'inner_rad': inner_radius_file, 'outer': outer_file,
                       'outer_rad': outer_radius_file, 'vol': volume}
@@ -67,41 +70,6 @@ class AirwayTree:
         self.vol_dims = vol_header.get_data_shape()
         self.vol_vox_dims = vol_header.get_zooms()
 
-        '''
-        The columns of the organised tree are:
-        branch: int
-            The branch ID
-        generation: int
-            The branch generation
-        parent: int
-            The branch ID of the Parent branch
-        children: list[int]
-            A list of branch IDs of the Children branches
-        points: list[(Tuple)]
-            A list of (x, y, z) tuples of branch points along centreline in voxels (not in mm)
-        centreline: list [(Tuple)]
-            A list of (x, y, z) tuples of branch points along centreline in millimeters
-        inner_radius: float
-            The branch lumen global (summary) radius in mm
-        inner_intensity: float
-            The branch lumen global (summary) intensity in HU
-        inner_samples: int
-            Number of samples used for global measurement
-        inner_area: float
-            The global luminal area of the branch in mm^2
-        inner_radii: list[float]
-            A list of non-smoothed measurements of luminal local radii
-        outer_radius: float
-            The branch total branch thickness global (summary) radius in mm
-        outer_intensity: float
-            The branch total branch thickness global (summary) intensity in HU
-        outer_samples: int
-            Number of samples used for global measurement
-        outer_area: float
-            The branch global total branch area measurement in mm^2
-        outer_radii: list[float]
-            A list of non-smoothed measurements of total branch thickness local radii.
-        '''
         self.tree = self.organise_tree()  #: Please see above for list of columns in airway tree dataframe.
 
     def get_airway_count(self) -> int:
@@ -127,3 +95,40 @@ class AirwayTree:
             x, y, z coordinates in millimeters
         """
         return point[0] * self.vol_vox_dims[0], point[1] * self.vol_vox_dims[1], point[2] * self.vol_vox_dims[2]
+
+    def get_branch(self, branch_id: int) -> pd.Series:
+        """
+
+        Parameters
+        ----------
+        branch_id: int
+            id value of the branch.
+
+        Returns
+        -------
+        branch series
+
+        See Also
+        -------
+        branch: The branch ID
+        generation: The branch generation
+        parent: The branch ID of the Parent branch
+        children: list[int] A list of branch IDs of the Children branches
+        points: list[(Tuple)] A list of (x, y, z) tuples of branch points along centreline in voxels (not in mm)
+        centreline: list [(Tuple)] A list of (x, y, z) tuples of branch points along centreline in millimeters
+        inner_radius: The branch lumen global (summary) radius in mm
+        inner_intensity: The branch lumen global (summary) intensity in HU
+        inner_samples: Number of samples used for global measurement
+        inner_area: The global luminal area of the branch in mm^1
+        inner_radii: list[float] A list of non-smoothed measurements of luminal local radii
+        outer_radius: The branch total branch thickness global (summary) radius in mm
+        outer_intensity: The branch total branch thickness global (summary) intensity in HU
+        outer_samples: Number of samples used for global measurement
+        outer_area: The branch global total branch area measurement in mm^1
+        outer_radii: list[float] A list of non-smoothed measurements of total branch thickness local radii
+        """
+        try:
+            return self.tree.loc[branch_id]
+        except KeyError as e:
+            print(f"No branch with id {e}.")
+            return None
