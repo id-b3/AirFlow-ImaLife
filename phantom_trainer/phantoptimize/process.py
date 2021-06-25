7898fd6efbf492232b2d37714c1be8d1bb8244fd
id-b3 <58172188+id-b3@users.noreply.github.com>
2021-06-25 10:22:23 +0200

    Make PhantomTrainer class Allows the training process to hold in memory the file paths and details about the current training instance.

--- phantom_trainer/phantoptimize/process.py
+++ phantom_trainer/phantoptimize/process.py
@@ -2,13 +2,12 @@ import subprocess
 from pathlib import Path
 from .split.compute_boundbox_regions import comp_bound_box
 from .split.split_segmentation_regions import split_seg_reg
-import tempfile
+from datetime import datetime
 import logging
 
 # Script constants
 opfrnt_spt = Path("./scripts/opfront_phantom_single.sh").resolve()
 measre_spt = Path("./scripts/measure_phantom_single.sh").resolve()
-log_out = Path("./phantom_logs/").resolve()
 
 # File constants
 p_surf_0 = "./phantom_volume_surface0.nii.gz"
@@ -17,42 +16,73 @@ p_surf_1 = "./phantom_volume_surface1.nii.gz"
 
 class PhantomTrainer:
     def __init__(self, p_vol: str, p_seg: str, out_dir: str):
+        """
+        Phantom Trainer class. Contains the information to repeatedly run the process_phantom method, which calculates
+        an error meaasure for a given set of parameters.
 
-        self.volume = Path(p_vol).resolve()
-        self.segmentation = Path(p_seg).resolve()
+        Parameters
+        ----------
+        p_vol: str
+            Phantom volume file
+        p_seg: str
+            Phantom segmentation file
+        out_dir: str
+            Output Directory for this training run.
+        """
+        self.volume = str(Path(p_vol).resolve())
+        self.segmentation = str(Path(p_seg).resolve())
         self.out_dir = Path(out_dir).resolve()
-        self.bound_box = self.out_dir / "common_files/boundboxes_split_regions_phantom.npy"
-        logging.basicConfig()
+        self.log_dir = self.out_dir / "logs" / f"training_log_{datetime.now()}.log"
+        self.bound_box = self.out_dir / "common_files" / "boundboxes_split_regions_phantom.npy"
 
+        logging.basicConfig(level=logging.DEBUG, filename=str(self.log_dir))
 
+    # TODO: Create a function that runs one loop of phantom opfront and measuring. Returns an error measure.
+    def process_phantom(self, run_number: int,
+                        op_par: str = "-i 15 -o 15 -I 2 -O 2 -b 0.4 -k 0.5 -r 0.7 -c 17 -e 0.7 -K 0",
+                        i_der: float = 0, o_der: float = 0, s_pen: float = 0) -> float:
+        """
+        A method that processes the phantom and calculates an error measure.
 
-# TODO: Create a function that runs one loop of phantom opfront and measuring. Returns an error measure.
-def process_phantom(proc_run: int, p_vol: str, p_seg: str,
-                    op_par: str = "-i 15 -o 15 -I 2 -O 2 -b 0.4 -k 0.5 -r 0.7 -c 17 -e 0.7 -K 0",
-                    i_der: float = 0, o_der: float = 0, s_pen: float = 0) -> float:
+        Parameters
+        ----------
+        run_number: int
+            The number of the current run.
+        op_par: str
+            Opfront Parameters
+        i_der: float
+            Inner derivative - test variable (range -1 to 1)
+        o_der: float
+            Outer derivative - test variable (range -1 to 1)
+        s_pen: float
+            Separation penalty - test variable (range 0 to 10)
 
-    err_m = 1  #: The error measure. Initialised to 1
-    parameters = f"{op_par} -F {i_der} -G {o_der} -d {s_pen}"
-    logging.info(f"Starting Phantom {p_vol} Training Run No.{proc_run} with parameters '{parameters}'\n"
-                 f"----------------------------------------------------------------\n")
-    # 1. run opfront with parameters VOL SEG OUT_DIR OPFRONT_PARAMS
-    logging.debug(f"Launching opfront for {p_vol} number {proc_run}...")
-    subprocess.run()
+        Returns
+        -------
+        The error measure for this set of opfront parameters.
+        """
+        err_m = 1  #: The error measure. Initialised to 1
+        parameters = f"{op_par} -F {i_der} -G {o_der} -d {s_pen}"
+        logging.info(f"Starting Phantom {self.volume} Training Run No.{run_number} with parameters '{parameters}'\n"
+                     f"----------------------------------------------------------------\n")
+        # 1. run opfront with parameters VOL SEG OUT_DIR OPFRONT_PARAMS
+        logging.debug(f"Launching opfront for {self.volume} number {run_number}...")
+        subprocess.run()
 
-    # 2. split the airways
-    if not Path.exists(Path()):
-        logging.debug(f"No bounding box found, computing bounding-box regions for run {proc_run}...")
-        comp_bound_box(p_seg, "./common_run/boundboxes_split_regions_phantom.npy")
+        # 2. split the airways
+        if not Path.exists(Path()):
+            logging.debug(f"No bounding box found, computing bounding-box regions for run {run_number}...")
+            comp_bound_box(self.segmentation, "./common_run/boundboxes_split_regions_phantom.npy")
 
-    logging.debug(f"Splitting results for opfront number {proc_run}...")
-    split_seg_reg()
-    # 3. measure the airways
-    logging.debug(f"Measuring results for run {proc_run}...")
-    subprocess.run([measre_spt, "VOL", "INNER_SURF", "OUTER_SURF", "OUT_FOLDER"])  # TODO CHANGE PLACEHOLDER INPUTS
-    # 4. merge the airways
-    logging.debug(f"Merging results for run {proc_run}...")
-    # 5. calculate the error measure
-    logging.debug(f"Calculating error measure for run {proc_run}...")
-    # return the error measure
-    logging.info(f"Error measure for {p_vol} run No. {proc_run} is: {err_m}")
-    return err_m
+        logging.debug(f"Splitting results for opfront number {run_number}...")
+        split_seg_reg()
+        # 3. measure the airways
+        logging.debug(f"Measuring results for run {run_number}...")
+        subprocess.run([measre_spt, "VOL", "INNER_SURF", "OUTER_SURF", "OUT_FOLDER"])  # TODO CHANGE PLACEHOLDER INPUTS
+        # 4. merge the airways
+        logging.debug(f"Merging results for run {run_number}...")
+        # 5. calculate the error measure
+        logging.debug(f"Calculating error measure for run {run_number}...")
+        # return the error measure
+        logging.info(f"Error measure for {self.volume} run No. {run_number} is: {err_m}")
+        return err_m
