4bcafff91b73402bc820c523169dbb16e4286ae4
id-b3 <58172188+id-b3@users.noreply.github.com>
2021-06-25 10:34:15 +0200

    Fix bug in measure_phantom_single.sh

--- phantom_trainer/phantoptimize/scripts/measure_phantom_single.sh
+++ phantom_trainer/phantoptimize/scripts/measure_phantom_single.sh
@@ -2,11 +2,11 @@
 # Created by Antonio:
 # Compute measurements on given airway lumen and outer wall segmentations. Receives original volume ($1) and input surface segmentations '.dcm' ($2, $3). Results are stored in ($4).
 # Added by Antonio: export paths to missing libraries needed by executables in ($5)
+# Modified by Ivan: Work with docker and newer opfront tools.
 
-if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ] || [ "$4" == "" ]
-then
-    echo Usage: "$0" VOLUME_FILE INIT_VOL_INNER_FILE INIT_VOL_OUTER_FILE OUTPUT_FOLDER
-    exit 1
+if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ] || [ "$4" == "" ]; then
+  echo Usage: "$0" VOLUME_FILE INIT_VOL_INNER_FILE INIT_VOL_OUTER_FILE OUTPUT_FOLDER
+  exit 1
 fi
 
 # INDPUT PARAMETERS
@@ -46,61 +46,63 @@ OUTER_RESULTS_PANDAS="${ROOT}_outer_local_pandas.csv"
 
 BRANCHES_PANDAS="${ROOT}_airways_centrelines.m"
 
+LOGFILE="${ROOT}.log"
+
 
 mkdir -p "$FOLDEROUT"
-
-echo -e "\n *** ${FILE_NO_EXTENSION} ***\n"
-echo -e "Volume: $VOL"
-echo -e "Inner Surface: $INNER_VOL"
-echo -e "Outer Surface: $OUTER_VOL"
-echo -e "Results folder: $FOLDEROUT\n"
-
+{
+  echo -e "\n *** ${FILE_NO_EXTENSION} ***\n"
+  echo -e "Volume: $VOL"
+  echo -e "Inner Surface: $INNER_VOL"
+  echo -e "Outer Surface: $OUTER_VOL"
+  echo -e "Results folder: $FOLDEROUT\n"
+} | tee "$LOGFILE"
 # ------------------------------------------------ EXECUTION STEPS ---------------------------------------
 
+{
+  echo -e "\nCreating mesh surface using marching cubes: inner surface"
 
-echo -e "\nCreating mesh surface using marching cubes: inner surface"
+  CALL="${BINARY_DIR}/img2gts -s $INNER_VOL -g $INNER_SURFACE"
+  echo -e "\n$CALL"
+  eval "$CALL"
 
-CALL="${BINARY_DIR}/img2gts -s $INNER_VOL -g $INNER_SURFACE"
-echo -e "\n$CALL"
-eval "$CALL"
+  echo -e "\nCreating mesh surface using marching cubes: outer surface"
 
-echo -e "\nCreating mesh surface using marching cubes: outer surface"
+  CALL="${BINARY_DIR}/img2gts -s $OUTER_VOL -g $OUTER_SURFACE"
+  echo -e "\n$CALL"
+  eval "$CALL"
 
-CALL="${BINARY_DIR}/img2gts -s $OUTER_VOL -g $OUTER_SURFACE"
-echo -e "\n$CALL"
-eval "$CALL"
+  echo -e "\nBinarising isotropic inner surface with threshold 1 for branch extraction:"
+  CALL="${BINARY_DIR}/imgconv -i $INNER_VOL -o $INNER_VOL_ISO_TH1 -t 0 -x 1"
 
-echo -e "\nBinarising isotropic inner surface with threshold 1 for branch extraction:"
-CALL="${BINARY_DIR}/imgconv -i $INNER_VOL -o $INNER_VOL_ISO_TH1 -t 0 -x 1"
+  echo -e "\n$CALL"
+  eval "$CALL"
 
-echo -e "\n$CALL"
-eval "$CALL"
+  # -- BRANCHES ----------------------------------
+  echo -e "\nComputing branches:"                          # this creates $BRANCHES_ISO
+  CALL="${BINARY_DIR}/be $INNER_VOL_ISO_TH1 -o $FOLDEROUT" # -vessels added (or use of OUTTER_VOL_ISO_TH14) for >1 iterations in the opfront (to allow for disconnectivity)
+  echo -e "\n$CALL"
+  eval "$CALL"
 
-# -- BRANCHES ----------------------------------
-echo -e "\nComputing branches:" # this creates $BRANCHES_ISO
-CALL="${BINARY_DIR}/be $INNER_VOL_ISO_TH1 -o $FOLDEROUT" # -vessels added (or use of OUTTER_VOL_ISO_TH14) for >1 iterations in the opfront (to allow for disconnectivity)
-echo -e "\n$CALL"
-eval "$CALL"
+  echo -e "\nMeasure inner surface:"
+  CALL="${BINARY_DIR}/gts_ray_measure -g $INNER_SURFACE -v $VOL -b $BRANCHES -o $INNER_RESULTS -l $INNER_RESULTS_LOCAL -p $INNER_RESULTS_PANDAS"
+  echo -e "\n$CALL"
+  eval "$CALL"
 
-echo -e "\nMeasure inner surface:"
-CALL="${BINARY_DIR}/gts_ray_measure -g $INNER_SURFACE -v $VOL -b $BRANCHES -o $INNER_RESULTS -l $INNER_RESULTS_LOCAL -p $INNER_RESULTS_PANDAS"
-echo -e "\n$CALL"
-eval "$CALL"
+  echo -e "\nMeasure outer surface:"
+  CALL="${BINARY_DIR}/gts_ray_measure -g $OUTER_SURFACE -v $VOL -b $BRANCHES -o $OUTER_RESULTS -l $OUTER_RESULTS_LOCAL -p $OUTER_RESULTS_PANDAS"
+  echo -e "\n$CALL"
+  eval "$CALL"
 
-echo -e "\nMeasure outer surface:"
-CALL="${BINARY_DIR}/gts_ray_measure -g $OUTER_SURFACE -v $VOL -b $BRANCHES -o $OUTER_RESULTS -l $OUTER_RESULTS_LOCAL -p $INNER_RESULTS_PANDAS"
-echo -e "\n$CALL"
-eval "$CALL"
+  echo -e "\n\nConvert branches to MATLAB readable format:"
+  CALL="${BINARY_DIR}/brh_translator $BRANCHES -pandas $BRANCHES_PANDAS"
+  echo -e "\n$CALL"
+  echo -e "DONE\n"
+  eval "$CALL"
 
-echo -e "\n\nConvert branches to MATLAB readable format:"
-CALL="${BINARY_DIR}/brh_translator $BRANCHES -pandas $BRANCHES_PANDAS"
-echo -e "\n$CALL"
-echo -e "DONE\n"
-eval "$CALL"
-
-# -- CLEAN UNNECESSARY FILES
-echo -e "\nClean unnecessary files:"
-CALL="rm $INNER_VOL_ISO $INNER_VOL_ISO_TH1 $BRANCHES_ISO"
-echo -e "\n$CALL"
-eval "$CALL"
-
+  # -- CLEAN UNNECESSARY FILES
+  echo -e "\nClean unnecessary files:"
+  CALL="rm $INNER_VOL_ISO $INNER_VOL_ISO_TH1 $BRANCHES_ISO"
+  echo -e "\n$CALL"
+  eval "$CALL"
+} | tee "$LOGFILE"
